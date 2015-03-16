301c0f0f9cf216e88be90db28c322df01c332c49
Kevin Greenman <kevin.greenman@gmail.com>
2015-03-15 23:28:17 -0700

    Create functions to read and concat data

--- README.md
+++ README.md
@@ -7,7 +7,7 @@ the unzipped files form the UCI HAR dataset. The working directory contains
 features.txt, activity_labels.txt and two sub-directories: test and train 
 where the data resides
 
-## Logical Flow of the Script
+# Logical Flow of the Script
 1. Define which columns to extract by parsing features.txt
 2. Read defined columns from train/x_train.txt into a dataframe
 3. Add the activity column to that dataframe using activity names
@@ -21,5 +21,9 @@ Two nice overviews of string functions in R can be found at [GastonSanchez](http
 
 ## Reading activity_labels.txt
 The function getActivityNames reads this file to create a dataframe describing the activities corresponding to the numbered activity.
+## Reading The Data in sub directories
+The function readData reads the data from the files passed in and creates a data frame. SubjectFile  is the file telling the subject who performed the activity. YFile is the label for the activity that was being performed in the original numeric format XFile is the actual data values for all columns. keepCol is the df created by getCols() that specifys which columns to keep. The files XFile, YFile, and SubjectFile all need to have the relative path specified (e.g. ./test/X_test.txt).  This function should be invoked twice, once for the test data and once for the training data. The function returns the resulting data frame with the human readable column labels.
+## Merging the two data frames
+The script buildData brings all the functions together. First it calls getCols() to make a data frame **dataCols** which contains the index and name of the columns to keep. Then buildData calls the readData function twice to build two data frames, **testdf** for the test data and **traindf** for the training data. These two data frames are concatenated together to make the **mergeddf** data frame. Finally, use the function getActivityNames to and use that data to add the activityName column to the mergeddf from the integers in activity column to make a more human readable format.
 
 
--- run_analysis.R
+++ run_analysis.R
@@ -22,8 +22,47 @@ getActivityNames<-function(){
                        colClasses=c("integer","character"))
   return(actNames)
 }
-
-
-## Collect labels for columns and activities
-dataCols<-getCols()
-actNames<-getActivityNames()
+#
+# The function readData reads the data from the files passed in creates a data frame. 
+# SubjectFile  is the file telling the subject who performed the activity. YFile is the 
+# label for the activity that was being performed in the original numeric format XFile 
+# is the actual data values for all columns. keepCol is the df created by getCols() 
+# that specifys which columns to keep.
+readData<-function(SubjectFile,XFile,YFile,keepCol){
+  # Read in the data then discard the unwanted columns
+  df<-read.table(XFile,header=FALSE)
+  df<-df[,keepCol$index]
+  # Now attach the activity column to the left side
+  activity<-read.table(YFile, header=FALSE)
+  df<-cbind(activity,df)
+  # Now attach the subject column to the left side
+  subject<-read.table(SubjectFile, header=FALSE)
+  df<-cbind(subject,df)
+  # Now make the column names human readable
+  colnames(df)<-c("subject","activity",keepCol$name)
+  return(df)
+}
+#
+# Build the merged data frame by calling functions See README.md for full description
+#
+buildData<-function(){
+  if ((!file.exists("./features.txt"))|(!file.exists("./test/X_test.txt"))){
+    stop("Error: Data files and subdirectories are not in the cwd!")
+  }
+  #get the columns to keep
+  dataCols<-getCols()
+  #read the two data frames and concatenate them
+  testdf<-readData("./test/subject_test.txt","./test/X_test.txt","./test/y_test.txt",
+                   dataCols)
+  traindf<-readData("./train/subject_train.txt","./train/X_train.txt","./train/y_train.txt",
+                    dataCols)
+  mergeddf<-rbind(testdf,traindf)
+  rm(testdf) #free memory now that we are done with this df 
+  rm(traindf)
+  # Use the integer activities to make a column with descriptive text for the activity
+  actNames<-getActivityNames()
+  activityName<-sapply(mergeddf[,2],function(x){actNames[x,2]})
+  activityName<-as.data.frame(activityName)
+  mergeddf<-cbind(mergeddf,activityName)
+  return(mergeddf)
+}
